35353b4ad8ccb1eafacf14bffd1393dc68d73829
derrick b <dbozich1@gmail.com>
2018-11-20 00:31:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,6 +12,18 @@ class App < Sinatra::Base
 
   post '/search' do
     binding.pry
+    nyt_api_key = 'f98593a095b44546bf4073744b540da0'
+    url = 'https://api.nytimes.com/svc/movies/v2/reviews/query=' + query +
+     '&api-key=' + nyt_api_key
+
+    res = Faraday.get url do |req|
+       req.params['api-key'] = nyt_api_key
+       # req.params['client_secret'] = client_secret
+       # req.params['v'] = '20160201'
+       # req.params['near'] = params[:zipcode]
+       # req.params['query'] = 'coffee shop'
+     end
+     binding.pry
   end
 
 end
