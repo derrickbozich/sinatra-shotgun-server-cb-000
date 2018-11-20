bda392977a9a3ceda674e1090ca4f9ca6052ad3a
derrick b <dbozich1@gmail.com>
2018-11-20 01:29:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,16 +10,27 @@ class App < Sinatra::Base
 
   post '/search' do
     query = params['q']
-    nyt_api_key = 'a071bd4ce142480c84ce0f54bc695e09'
-    url = 'https://api.nytimes.com/svc/movies/v2/reviews/query=' + query +
-     '&api-key=' + nyt_api_key
-    endpoint = 'https://api.nytimes.com/svc/movies/v2/reviews/'
+    # nyt_api_key = 'a071bd4ce142480c84ce0f54bc695e09'
+    # url = 'https://api.nytimes.com/svc/movies/v2/reviews/query=' + query +
+    #  '&api-key=' + nyt_api_key
+    # endpoint = 'https://api.nytimes.com/svc/movies/v2/reviews/'
+    # 
+    # 
+    # res = Faraday.get endpoint do |req|
+    #    req.params['api-key'] = nyt_api_key
+    #    req.params['query'] = query
+    #  end
 
-
-    res = Faraday.get endpoint do |req|
-       req.params['api-key'] = nyt_api_key
-       req.params['query'] = query
-     end
+    uri = URI("https://api.nytimes.com/svc/search/v2/articlesearch.json")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    uri.query = URI.encode_www_form({
+      "api-key" => "a071bd4ce142480c84ce0f54bc695e09",
+      "q" => query
+    })
+    request = Net::HTTP::Get.new(uri.request_uri)
+    @result = JSON.parse(http.request(request).body)
+    puts @result.inspect
 
 
 
