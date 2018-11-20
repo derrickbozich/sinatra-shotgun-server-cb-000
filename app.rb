0cd21840d19f7faa49efae24306cbd4a3e693355
derrick b <dbozich1@gmail.com>
2018-11-20 01:26:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ class App < Sinatra::Base
 
   post '/search' do
     query = params['q']
-    nyt_api_key = 'f98593a095b44546bf4073744b540da0'
+    nyt_api_key = 'a071bd4ce142480c84ce0f54bc695e09'
     url = 'https://api.nytimes.com/svc/movies/v2/reviews/query=' + query +
      '&api-key=' + nyt_api_key
     endpoint = 'https://api.nytimes.com/svc/movies/v2/reviews/'
 
-    
+
     res = Faraday.get endpoint do |req|
        req.params['api-key'] = nyt_api_key
        req.params['query'] = query
