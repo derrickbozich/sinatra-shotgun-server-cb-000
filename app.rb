ef5773c146a87f1fa4fd5b2275aaa1a3c6c00a75
derrick b <dbozich1@gmail.com>
2018-11-20 00:41:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,6 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  # get '/' do
-  #   "Started my server using edit Shotgun! edit"
-  # end
-
   get '/' do
     send_file './public/index.html'
   end
