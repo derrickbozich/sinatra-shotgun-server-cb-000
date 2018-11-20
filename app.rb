114260a851fa730fae0a8cacc5dcfcb30007e30e
derrick b <dbozich1@gmail.com>
2018-11-20 00:50:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'json'
 
 class App < Sinatra::Base
 
@@ -11,7 +12,7 @@ class App < Sinatra::Base
     nyt_api_key = 'f98593a095b44546bf4073744b540da0'
     url = 'https://api.nytimes.com/svc/movies/v2/reviews/query=' + query +
      '&api-key=' + nyt_api_key
-    binding.pry
+    
     res = Faraday.get url do |req|
        #  req.params['api-key'] = nyt_api_key
        # req.params['client_secret'] = client_secret
@@ -19,8 +20,9 @@ class App < Sinatra::Base
        # req.params['near'] = params[:zipcode]
        # req.params['query'] = 'coffee shop'
      end
-     binding.pry
-     res.to_json
+
+     JSON.parse(res)
+
      binding.pry
   end
 
