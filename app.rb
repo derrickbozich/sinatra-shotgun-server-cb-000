454b87fd179f42cfba460432d0f96639061eb096
derrick b <dbozich1@gmail.com>
2018-11-20 01:01:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,6 +25,7 @@ class App < Sinatra::Base
 
 
     url = 'https://api.spotify.com/v1/search?type=artist&q=' + query
+    binding.pry
     response = RestClient.get(url)
     data = JSON.parse(response)
 
