ca196249ea518efd73306b97357b62215f23f44f
derrick b <dbozich1@gmail.com>
2018-11-20 00:59:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/search' do
-    # query = params['q']
+    query = params['q']
     # nyt_api_key = 'f98593a095b44546bf4073744b540da0'
     # url = 'https://api.nytimes.com/svc/movies/v2/reviews/query=' + query +
     #  '&api-key=' + nyt_api_key
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     #    # req.params['query'] = 'coffee shop'
     #    req.response :json
     #  end
-  
+
 
     url = 'https://api.spotify.com/v1/search?type=artist&q=tycho'
     response = RestClient.get(url)
